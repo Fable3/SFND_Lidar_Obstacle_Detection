e8609bbaebfe8a8e98a22ca0f791b31c95567c25
Blahut, György <fable3@gmail.com>
2021-02-20 11:58:29 +0100

    non-recursive kdtree

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -29,25 +29,27 @@ struct KdTree
 	{
 		// TODO: Fill in this function to insert a new point into the tree
 		// the function should create a new node and place correctly with in the root 
-		insertHelper(point, id, 0, root);
+		int depth = 0;
+		Node **target_node = &root;
+		for (;;)
+		{
+			int d = depth % 2;
+			if (*target_node == NULL)
+			{
+				*target_node = new Node(point, id);
+				break;
+			}
+			else if (point[d] < (*target_node)->point[d])
+			{
+				target_node = &(*target_node)->left;
+			}
+			else
+			{
+				target_node = &(*target_node)->right;
+			}
+			depth++;
+		}
 	}
-	void insertHelper(const std::vector<float> &point, int id, int depth, Node *&target_node)
-	{
-		int d = depth % 2;
-		if (target_node == NULL)
-		{
-			target_node = new Node(point, id);
-		}
-		else if (point[d] < target_node->point[d])
-		{
-			insertHelper(point, id, depth + 1, target_node->left);
-		}
-		else
-		{
-			insertHelper(point, id, depth + 1, target_node->right);
-		}
-	}
-
 
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
